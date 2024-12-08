73633c91789d94d6988fac16a37cf68eaabbdddd
luiz frança <kazumi200516@gmail.com>
2024-12-08 13:04:59 -0300

    calcular padrao (implementar explicacaoa)

--- calculadoras_multiplas/explicando/explicacao.py
+++ calculadoras_multiplas/explicando/explicacao.py
@@ -56,3 +56,25 @@ else:
     exit()
 calculo_montante = capital + resultado_tempo
 print("seu montante:", calculo_montante)    
+
+# CALCULADORA PADRAO
+numero_digite = int(input("digite aqui seu primeiro numero"))
+numero_digite2 = int(input("digite aqui seu primeiro numero"))
+
+Mult = numero_digite * numero_digite2
+Soma = numero_digite + numero_digite2
+Div = numero_digite / numero_digite2 
+Sub = numero_digite - numero_digite2
+print("ESCOLHA PARA REALIZAR A SOMA + / * -")
+escolha = int(input("escolha para realizar: "))
+if escolha == "+":
+    print(Soma)
+elif escolha == "-":
+    print(Sub)
+elif escolha == "/":
+    print(Div)
+elif escolha == "*":
+    print(Mult)
+else:
+    print("não foi encontrado, tente novamente mais tarde!")
+    exit()
