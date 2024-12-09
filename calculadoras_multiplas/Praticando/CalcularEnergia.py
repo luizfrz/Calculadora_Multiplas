1c07df2f120146cdb1680fc92537d4eb56ac57ce
luiz frança <kazumi200516@gmail.com>
2024-12-09 20:04:25 -0300

    calcular energia

--- calculadoras_multiplas/Praticando/CalcularEnergia.py
+++ calculadoras_multiplas/Praticando/CalcularEnergia.py
@@ -10,7 +10,7 @@
 """
 print("seja bem vindo! sistema para calcular consumo da sua energia")
 deseja_acessar = str(input("Deseja acessar o sistema? SIM / NÃO"))
-if deseja_acessar.lower () == "SIM":
+if deseja_acessar.lower () == "sim":
     print("vamos descobrir quanto voce fez de consumo!")
 else: 
     print("opa, voce pode voltar quanto quiser!")
@@ -19,7 +19,12 @@ else:
 mensagem_ex = "potencia deve ser digitada sempre primeiro numero após virgula"
 mensagem_ex2 = "dessa maneira 5000 = 5,0"
 potencia = float(input("digite aqui potencia de energia (w)"))
-tempo = input("digite aqui seu tempo")
+tempo = int(input("digite aqui seu tempo"))
 tempo_li = 60
 conversao = tempo / tempo_li
+calculo_energa = conversao * potencia
+mensagem_abaixo = "aqui esta seu consumo de energia"
+print(mensagem_abaixo, calculo_energa)
+
+
 
